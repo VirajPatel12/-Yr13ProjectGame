934ee4b0bd7db617af843ea4c6f64ed8a28f1fb1
Viraj Patel <patelv@scotscollege.school.nz>
2022-09-01 15:19:32 +1200

    more work

--- Main.py
+++ Main.py
@@ -4,41 +4,42 @@ import os
 import time
 import random
 import tkinter
+from pygame.locals import *
+pygame.font.init()
 main = tkinter.Tk()
 main.title("Space Invaders")
 main.geometry('1000x1000')
 # adding extra wedgets below
 main.mainloop()
 
-# loading icons/assets for the game (paste images)(os.path.join)
-red_ship = 
-pink_ship = 
-yellow_ship =
 
-# player ship (main own ship)
+# Background load image command + scales it according to dimensions.
+BACKGROUND = pygame.image.load('Background.png')
 
-blue_ship = 
-
-# Bullets
-
-blue_bullets = 
-pink_bullets = 
-yellow_bullets = 
-red_bullets = 
-
-# Background load image
-BG = color()
-
+#create screen 
+screen = pygame.display.set_mode((800, 600))
+#list variables below
+running = True
+while running:
+    screen.blit(BACKGROUND, (0, 0))
 
 # Clock Object
 
 def main():
     run = True
     FPS = 60
+    level = 1
+    lives = 5
+    main_font = pygame.font.SysFont("Arial", 50, bold=False, italic=False)
     clock = pygame.time.Clock()
 
     def redraw_tab(): #updates 60 timmes per second drawings inside fuction
-        
+        WIN.blit(BG, (0,0))
+        lives_label = main_font.render(f"Lives: {lives}", 1, (255,255,255 ))
+        level_label = main_font.render(f"Level: {level}", 1, (255,255,255 ))   
+
+        WIN.blit(lives_label, (10, 10))   
+        WIN.blit(level_label, (WIDTH - level_label.get_width() - 10, 10)) 
 
         pygame.display.update()
 
