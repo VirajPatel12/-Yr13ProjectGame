83a1d5bd885b92f3b6bd9abf2e04804cd38e7123
Viraj Patel <patelv@scotscollege.school.nz>
2022-10-18 10:09:27 +1300

    Further making

--- Main.py
+++ Main.py
@@ -16,6 +16,7 @@ game_paused = False
 
 # Define Fonts
 font = pygame.font.SysFont("arialblack", 40)
+font2 = pygame.font.SysFont(None, 40)
 
 # Define Colour
 TEXT_COL = (255, 255, 255)
@@ -28,8 +29,22 @@ quit_image = pygame.image.load("QuitButton.png").convert_alpha()
 resume_button = ButtonClass.Button(0, 0, resume_image, 1)
 quit_button = ButtonClass.Button(430, 400, quit_image, 1)
 
+# define temporary variables
 line_width = 6
 markers = []
+clicked = False
+pos = []
+player = 1
+winner = 0
+game_over = False
+pos = (0, 0)
+
+# colours
+
+green = (0, 255, 0)
+red = (255, 0, 0)
+blue = (0, 0, 255)
+
 def draw_grid():
     background_colour = (28, 170, 156)
     grid = (50, 50, 50)
@@ -42,6 +57,54 @@ def draw_text(text, font, text_col, x, y):
     img = font.render(text, True, text_col)
     screen.blit(img, (x, y))
 
+def draw_markers():
+	x_pos = 0
+	for x in markers:
+		y_pos = 0
+		for y in x:
+			if y == 1:
+				pygame.draw.line(screen, red, (x_pos * 100 + 15, y_pos * 100 + 15), (x_pos * 100 + 85, y_pos * 100 + 85), line_width)
+				pygame.draw.line(screen, red, (x_pos * 100 + 85, y_pos * 100 + 15), (x_pos * 100 + 15, y_pos * 100 + 85), line_width)
+			if y == -1:
+				pygame.draw.circle(screen, green, (x_pos * 100 + 50, y_pos * 100 + 50), 38, line_width)
+			y_pos += 1
+		x_pos += 1
+
+def check_winner():
+
+    global winner
+    global game_over
+
+    y_pos = 0
+    for x in markers:
+
+        if sum(x) == 3:
+            winner = 1
+            game_over = True
+
+        if sum(x) == 3:
+            winner = 2
+            game_over = True
+
+            # row check
+        if markers[0][y_pos] + markers[1][y_pos] + markers[2][y_pos] == 3:
+            winner = 1
+            game_over = True
+
+        if markers[0][y_pos] + markers[1][y_pos] + markers[2][y_pos] == -3:
+            winner = 2
+            game_over = True
+        y_pos += 1
+
+    if markers[0][0] + markers[1][1] + markers[2][2] == 3 or markers[2][0] + markers[1][1] + markers[2][2] == 3:
+        winner = 1
+        game_over = True
+
+    if markers[0][0] + markers[1][1] + markers[2][2] == -3 or markers[2][0] + markers[1][1] + markers[2][2] == -3:
+        winner = 2
+        game_over = True
+
+
 def draw_bg():
     screen.fill((100, 100, 100))
 
@@ -49,16 +112,18 @@ for x in range(3):
     row = [0] * 3
     markers.append(row)
 
-print(markers)
+print(markers) # lists different numbers
 
-
+def draw_winner():
+    win_text = 'Player' + str(winner) + " Wins!"
 
 run = True
 while run:
     # Background Colour (function to change colour of screen)
     draw_grid()
+    draw_markers()
 
-    if game_paused == True:
+    if game_paused == True: # below makes space = pause, which puts a menu.
         draw_bg()
         if resume_button.draw(screen):
             game_paused = False
@@ -75,6 +140,21 @@ while run:
                 print("Pause")
         if event.type == pygame.QUIT:
             run = False
+            # This is a click cycle, so when you click/release, a variable becomes true/false
+        if game_over == 0:
+            if event.type == pygame.MOUSEBUTTONDOWN and clicked == False:
+                clicked = True
+            if event.type == pygame.MOUSEBUTTONUP and clicked == True:
+                clicked = False
+                pos = pygame.mouse.get_pos() # Gives mouse coordinates/location
+                cell_x = pos[0]
+                cell_y = pos[1]
+                if markers[cell_x // 100][cell_y // 100] == 0:
+                    markers[cell_x // 100][cell_y // 100] = player
+                    player *= -1
+                    check_winner()
+
+
 
         pygame.display.update()
 
